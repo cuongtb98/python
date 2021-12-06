ae32ac22bfc89913d8730803268d4808e59b6d69
cuong <cuong98nguyen@gmail.com>
2021-12-06 12:43:46 +0700

    update mongodb basic

--- tkinter/main.py
+++ tkinter/main.py
@@ -1,25 +1,31 @@
 from tkinter import *
 from tkinter import ttk
 root = Tk()
-
+e = Entry(root)
+e.grid(row=5)
 # ============= defind variable =============
 # Lable
 Label1 = Label(root, text="Hello Cuong")
-Label2 = Label(root, text="Do you like Python")
-Label3 = Label(root, text="Let go")
+
+# function
+def getName():
+    Label_get_name = Label(root, text="Hello: " + e.get())
+    Label_get_name.grid()
 
 # button
-Button1 = Button(root, text="Click me!")
-Button2 = Button(root, text="Quit")
+Button1 = Button(root, text="Click me!", command=getName)
+Button2 = Button(root, text="Quit", command=root.destroy)
 
 # ============= end defind variable =============
 
 Button1.grid(column=0, row=0)
+
+
+Label1.grid(column=0, row=1)
+
+
+
+
 Button2.grid(column=0, row=2)
-# Label1.grid(column=0, row=0)
-Label2.grid(column=0, row=1)
-# Label3.grid(column=0, row=2)
-
-
 root.geometry("200x200")
 root.mainloop()
